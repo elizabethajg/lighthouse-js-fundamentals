0dfae0efc526c91b90291a8c4231655bdad7d80a
Elizabeth Giesbrecht <elizabethgiesbrecht@outlook.com>
2022-05-15 12:03:39 -0700

    Update loopyLighthouse with comments

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -1,10 +1,13 @@
 for(let i = 100; i < 201; i++){
+  //Check first if i is a multiple of 3 and 4
   if(i % 3 === 0 && i % 4 === 0){
     console.log("LoopyLighthouse");
+  //Now check 3 or 4 individually
   }else if(i % 3 === 0){
     console.log("Loopy");
   }else if(i % 4 === 0){
     console.log("Lighthouse");
+  //If divisible by neither, print number
   }else{
     console.log(i);
   }
